187d70485064dbf827f99ca59a62bca7d7a16e99
Mohit Yadav <mohit111198@gmail.com>
2019-07-05 14:02:36 +0530

    Split into Train and Test sets

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -25,3 +25,7 @@ transform = ColumnTransformer([("countries", OneHotEncoder(), [0])], remainder="
 X = transform.fit_transform(X)
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
+
+#Splitting dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train ,X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
