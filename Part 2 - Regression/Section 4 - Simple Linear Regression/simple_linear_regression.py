3fddf4ee0ac2210fcf4d3006e5d4375b4cf42a1d
Mohit Yadav <mohit111198@gmail.com>
2019-07-10 14:27:17 +0530

    SLR

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -21,3 +21,27 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting Simple Linear Regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) 
+
+#Predicting test set
+y_pred = regressor.predict(X_test)
+
+#Visualizing training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
