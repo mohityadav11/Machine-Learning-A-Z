35eadd394f3371caa2808642224f7218d8990e96
Mohit Yadav <mohit111198@gmail.com>
2019-07-11 14:57:56 +0530

    MLR

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -37,3 +37,6 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+#Predicting the Test set results
+y_pred = regressor.predict(X_test)
