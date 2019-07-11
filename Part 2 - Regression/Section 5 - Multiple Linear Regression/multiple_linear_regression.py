63a216fa2d62cb768ae4a2d7194f244f5d4a3baf
Mohit Yadav <mohit111198@gmail.com>
2019-07-11 16:08:15 +0530

    MLR

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -40,3 +40,7 @@ regressor.fit(X_train, y_train)
 
 #Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Building optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr= np.ones((50,1)).astype(int), values=X, axis=1)
