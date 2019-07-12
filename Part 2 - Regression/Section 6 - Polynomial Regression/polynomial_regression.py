ba04de05019580400d4c03003bb46f6b3539a39b
Mohit Yadav <mohit111198@gmail.com>
2019-07-12 13:55:09 +0530

    Polynomial Reg

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2:3].values
+y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.model_selection import train_test_split
@@ -49,3 +49,9 @@ plt.title('Truth or Bluff(Polynomial Regression)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
+
+#Predicting new result with Linear regression
+lin_reg.predict([[6.5]])
+
+#Predicting new result with Linear regression
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
