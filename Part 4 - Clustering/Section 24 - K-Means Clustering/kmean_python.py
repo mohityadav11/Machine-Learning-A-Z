f87c7cd99a60d17f91352987fde412bd0d590416
Mohit Yadav <mohit111198@gmail.com>
2019-08-05 19:21:23 +0530

    KMean

---
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmean_python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 18:59:57 2019
+
+@author: mohityadav
+"""
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+#Importing mall dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+
