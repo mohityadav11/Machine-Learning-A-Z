42c7541e9af4c79e41ddad86e00f43565d162dcb
Mohit Yadav <mohit111198@gmail.com>
2019-08-08 22:55:47 +0530

    UCB

---
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 22:54:06 2019
+
+@author: mohityadav
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
