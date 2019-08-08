cd0ac60d5fd9eb91125d14f3488b48717578e4b0
Mohit Yadav <mohit111198@gmail.com>
2019-08-08 14:23:56 +0530

    Apriori

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -17,3 +17,11 @@ for i in range(0 ,7501):
     
 #Training Apriori on the dataset
 from apyori import apriori
+rules = apriori(transactions,min_support=0.003, min_confidence=0.2,min_lift =3,min_length=2)
+
+#Visuaizing the results
+results = list(rules)
+
+results_list = []
+for i in range(0, len(results)):
+    results_list.append('\RESULTS:\t' + str(results[i][2]))
