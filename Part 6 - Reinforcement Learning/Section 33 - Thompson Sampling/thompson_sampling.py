473c86764112c0bf1e25168530dd3041e7d046c2
Mohit Yadav <mohit111198@gmail.com>
2019-08-09 22:49:51 +0530

    TS

--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling.py
@@ -14,30 +14,27 @@ import pandas as pd
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 #Implementing Thompson Sampling
-import math
+import random
 N = 10000
 d = 10
 ads_selected = []
-numbers_of_selection = [0] * d
-sums_of_rewards = [0] * d
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
 total_reward = 0
 for n in range(0, N):
     ad = 0
-    max_upper_bound = 0
+    max_random = 0
     for i in range(0, d):
-        if (numbers_of_selection[i] > 0):
-            average_reward = sums_of_rewards[i] / numbers_of_selection[i]
-            delta_i = math.sqrt(3/2 * math.log(n+1) / numbers_of_selection[i])
-            upper_bound = average_reward + delta_i
-        else:
-            upper_bound = 1e400
-        if upper_bound > max_upper_bound:
-            max_upper_bound = upper_bound
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
             ad = i
     ads_selected.append(ad)
-    numbers_of_selection[ad] =  numbers_of_selection[ad] + 1
     reward = dataset.values[n ,ad]
-    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+    if reward == 1:
+        numbers_of_rewards_1[ad] =  numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] =  numbers_of_rewards_0[ad] + 1
     total_reward = total_reward + reward
 
 #Visualizing the results
