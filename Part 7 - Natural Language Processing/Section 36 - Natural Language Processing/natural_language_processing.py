1acef70a52e93b9888a1e095a27337abf847a8f6
Mohit Yadav <mohit111198@gmail.com>
2019-08-10 15:02:08 +0530

    NLP

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -9,3 +9,16 @@ Created on Sat Aug 10 13:40:46 2019
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+#Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
+review = review.lower()
+review = review.split()
+review = [word for word in review if not word in stopwords.words('english')]
