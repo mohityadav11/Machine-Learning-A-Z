41b0638bf073fba3526284ec2fc9819d536dbeca
Mohit Yadav <mohit111198@gmail.com>
2019-08-10 15:14:03 +0530

    NLP

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -18,7 +18,9 @@ import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
 review = review.lower()
 review = review.split()
-review = [word for word in review if not word in stopwords.words('english')]
+ps = PorterStemmer()
+review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
