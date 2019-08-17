6eaaadaedff183deea9a2e277ae3ff79b4bed735
Mohit Yadav <mohit111198@gmail.com>
2019-08-17 17:41:11 +0530

    PCA

--- Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
+++ Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
@@ -12,13 +12,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
