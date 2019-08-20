ae6b2bf112f97cf281fda5c3dea6f238b2084ba4
Mohit Yadav <mohit111198@gmail.com>
2019-08-20 23:55:22 +0530

    kpca

--- Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/kernel_pca.py
+++ Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/kernel_pca.py
@@ -15,6 +15,7 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
@@ -24,6 +25,12 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Applying Kernel PCA
+from sklearn.decomposition import KernelPCA
+kpca = KernelPCA(n_components=2, kernel='rbf')
+X_train = kpca.fit_transform(X_train)
+X_test = kpca.transform(X_test)
 
 #Fitting Logistic regression to Training Set
 from sklearn.linear_model import LogisticRegression
