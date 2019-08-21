10d541ea93413e4dcf62680d6115369221d31183
Mohit Yadav <mohit111198@gmail.com>
2019-08-21 19:51:56 +0530

    xgb

--- Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xg_boost_ex.py
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xg_boost_ex.py
@@ -29,3 +29,21 @@ X = X[: , 1:]
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Fitting XGBoost to the training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#Applying K-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier,X=X_train,y=y_train,cv=10)
+accuracies.mean()
+accuracies.std()
